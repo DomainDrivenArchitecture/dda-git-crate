83fe293f3d4bfb6f9b0ed550ec98d8da6f6e7eaa
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-03-13 17:51:07 +0100

    initialized project

---
+++ project.clj
@@ -0,0 +1,33 @@
+(defproject dda/dda-git-crate "0.1.0-SNAPSHOT"
+  :description "module for cloning and managing git repositories & server conectivity."
+  :url "https://domaindrivenarchitecture.org"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.palletops/pallet "0.8.12" :exclusions [org.clojure/tools.cli]]
+                 [com.palletops/stevedore "0.8.0-beta.7"]
+                 [prismatic/schema "1.1.3"]
+                 [com.palletops/git-crate "0.8.0-alpha.2" :exclusions [org.clojure/clojure]]
+                 [org.domaindrivenarchitecture/dda-pallet-commons "0.3.1"]
+                 [org.domaindrivenarchitecture/dda-pallet "0.3.1"]]
+  :profiles {:dev
+             {:source-paths ["integration"]
+              :resource-paths ["dev-resources"]
+              :dependencies
+              [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
+               [com.palletops/pallet "0.8.12" :classifier "tests"]
+               [org.domaindrivenarchitecture/dda-pallet-commons "0.3.1" :classifier "tests"]
+               [ch.qos.logback/logback-classic "1.1.8"]
+               [org.slf4j/jcl-over-slf4j "1.7.22"]]
+              :plugins
+              [[lein-sub "0.3.0"]]}
+              :leiningen/reply
+               {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
+                :exclusions [commons-logging]}}
+   :local-repo-classpath true
+   :repositories [["snapshots" :clojars]
+                  ["releases" :clojars]]
+   :deploy-repositories [["snapshots" :clojars]
+                         ["releases" :clojars]]
+   :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
+                         :resource-paths ^:replace ["dev-resources"]}})
