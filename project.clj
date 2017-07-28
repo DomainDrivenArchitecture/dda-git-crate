0c45f64d42e10c71e09f61380d51bb5aa2e0a15d
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-07-28 16:50:42 +0200

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-git-crate "0.1.1"
+(defproject dda/dda-git-crate "0.1.2-SNAPSHOT"
   :description "Module for cloning and managing git repositories & server conectivity."
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
