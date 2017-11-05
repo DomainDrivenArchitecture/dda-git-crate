8a80b6d30e766c4f47d737104499cbbdd57b82c1
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-11-05 13:07:24 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [dda/dda-pallet "0.5.5"]
+                 [dda/dda-pallet "0.6.1"]
                  [dda/dda-serverspec-crate "0.2.2"]
                  [com.palletops/git-crate "0.8.0-alpha.2" :exclusions [org.clojure/clojure]]]
   :repositories [["snapshots" :clojars]
@@ -16,13 +16,13 @@
                    :dependencies
                     [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
                      [com.palletops/pallet "0.8.12" :classifier "tests"]
-                     [dda/dda-user-crate "0.6.2"]
+                     [dda/dda-user-crate "0.7.0"]
                      [ch.qos.logback/logback-classic "1.2.3"]
-                     [org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
+                     [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                     :plugins
                     [[lein-sub "0.3.0"]]
                     :leiningen/reply
-                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
+                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                      :exclusions [commons-logging]}}}
    :local-repo-classpath true
    :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
