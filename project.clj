e9f8467c0d3dd4cae61e1c36961ce64da6d76751
jem <michael.jerger@meissa-gmbh.de>
2020-07-03 17:56:10 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "3.0.0"]
+  :dependencies [[dda/dda-pallet "3.1.1"]
                  [dda/dda-serverspec-crate "1.3.4"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
@@ -18,7 +18,7 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0"]
+                   [[org.clojure/test.check "1.0.0"]
                     [dda/pallet "0.9.1" :classifier "tests"]
                     [dda/data-test "0.1.1"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
@@ -31,13 +31,13 @@
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
                     :dependencies [[dda/pallet "0.9.1" :classifier "tests"]
-                                   [org.clojure/test.check "0.10.0"]]}
+                                   [org.clojure/test.check "1.0.0"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-git-crate.main
                        :uberjar-name "dda-git-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.4.2"]
+                       :dependencies [[org.clojure/tools.cli "1.0.194"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]}}
