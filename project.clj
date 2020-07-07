e087e3ceb01ac88362ba26c4af6364471b595d6c
jem <michael.jerger@meissa-gmbh.de>
2020-07-07 16:50:14 +0200

    Version 2.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-git-crate "2.2.3-SNAPSHOT"
+(defproject dda/dda-git-crate "2.2.3"
   :description "Module for cloning and managing git repositories & server conectivity."
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
